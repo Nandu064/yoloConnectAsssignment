80c0f10de265874eb4b51342214cffed8e6d37c1
Nandu064 <harish861432@gmail.com>
2021-06-19 01:22:15 +0530

    committed by adding pagination

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,21 +1,72 @@
 import axios from 'axios'
 import {useState,useEffect} from 'react'
 import {Table,Modal,Card} from 'react-bootstrap'
+import BootstrapTable from 'react-bootstrap-table-next';
+import paginationFactory from 'react-bootstrap-table2-paginator';
 import React from 'react'
 export default function Dashboard() {
    const [launches,setLaunches] = useState();
    const [show, setShow] = useState(false);
    const [selectedData, setSelectedData] = useState();
-   //const {flight_number,details,mission_name,launch_date_utc,launch_success} = selectedData
+   const launchData = launches ? launches.map(launch=>{
+       if(launch.launch_success === true){
+            return launch.launch_success = "Success"
+       }else if(launch.launch_success === false){
+            return launch.launch_success = "Failed"
+       }
+       else if(launch.launch_success === null){
+            return launch.launch_success = "Upcoming"
+       }return null;
+   }): null;
+   const paginateData = launches
+   const paginateColumns = [
+    { dataField: 'flight_number', text: 'No',headerStyle: { backgroundColor: '#215E95', color: 'white'} },
+    { dataField: 'launch_date_utc', text: 'Launched (UTC)',headerStyle: { backgroundColor: '#215E95', color: 'white'} },
+    { dataField: 'launch_site.site_name', text: 'Location',headerStyle: { backgroundColor: '#215E95', color: 'white'} },
+    { dataField: 'mission_name', text: 'Mission',headerStyle: { backgroundColor: '#215E95', color: 'white'} },
+    { dataField: 'rocket.second_stage.payloads[0].orbit', text: 'Orbit',headerStyle: { backgroundColor: '#215E95', color: 'white'} },
+    { 
+        dataField: 'launch_success',
+        style:(cell,row,rowIndex,colIndex)=>{
+            if(row.launch_success === "Success"){
+                return {
+                    backgroundColor: 'lightgreen',
+                    color:'green'
+                }
+            }else if(row.launch_success === "Failed"){
+                return {
+                    backgroundColor:'orange',
+                    color:'red'
+                }
+            }else if(row.launch_success === "Upcoming"){
+                return {
+                    backgroundColor:'orange',
+                    color:'red'
+                }
+            }
+        },
+        text: 'Launch Status',headerStyle: { backgroundColor: '#215E95', color: 'white'} },
+    { dataField: 'rocket.rocket_name', text: 'Rocket',headerStyle: { backgroundColor: '#215E95', color: 'white'} },
+  ];
+  const pagination = paginationFactory({
+    page: 1,
+    sizePerPage: 10,
+    lastPageText: '>>',
+    firstPageText: '<<',
+    nextPageText: '>',
+    prePageText: '<',
+    showTotal: true,
+    alwaysShowAllBtns: true,
+  });
+  const rowEvent={
+    onClick: (e, row, rowIndex) => {
+        setShow(true);
+        setSelectedData(launches[rowIndex]);
+      }
+  }
    const handleClose = ()=> setShow(false)
     useEffect(() => {
-        axios.get('https://api.spacexdata.com/v3/launches'
-        ,{
-            params: {
-                limit:10
-              }
-        }
-        )
+        axios.get('https://api.spacexdata.com/v3/launches')
         .then((res)=>{
             setLaunches(res.data);
             console.log(res.data);
@@ -33,7 +84,6 @@ export default function Dashboard() {
                 ,{
                     params: {
                         launch_success: false,
-                        limit:10
                     }
                 }
                 )
@@ -46,7 +96,6 @@ export default function Dashboard() {
                 ,{
                     params: {
                         launch_success: !false,
-                        limit:10
                     }
                 }
                 )
@@ -55,25 +104,13 @@ export default function Dashboard() {
                 })
         }
         if(selectedValue==="upcoming"){
-            axios.get('https://api.spacexdata.com/v3/launches/upcoming'
-                ,{
-                    params: {
-                        limit:10
-                    }
-                }
-                )
+            axios.get('https://api.spacexdata.com/v3/launches/upcoming')
                 .then((res)=>{
                     setLaunches(res.data);
                 })
         }
         if(selectedValue==="all"){
-            axios.get('https://api.spacexdata.com/v3/launches'
-                ,{
-                    params: {
-                        limit:10
-                    }
-                }
-                )
+            axios.get('https://api.spacexdata.com/v3/launches')
                 .then((res)=>{
                     setLaunches(res.data);
                 })
@@ -111,49 +148,8 @@ export default function Dashboard() {
                     </select>
                 </div>
             </div>
-           
-            <Table responsive>
-                <thead>
-                    <tr style={{backgroundColor:'lightgray'}}>
-                        <th>No</th>
-                        <th>Launched (UTC)</th>
-                        <th>Location</th>
-                        <th>Mission</th>
-                        <th>Orbit</th>
-                        <th>Launch Status</th>
-                        <th>Rocket</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        launches ? 
-                        launches.map((launch,index)=>{
-                            return(
-                                <tr 
-                                    key={index}
-                                    onClick={()=>{
-                                        setShow(true)
-                                        setSelectedData(launch)
-                                        }}
-                                >
-                                    <td>{index+1}</td>
-                                    <td>{launch.launch_date_utc}</td>
-                                    <td>{launch.launch_site.site_name}</td>
-                                    <td>{launch.mission_name}</td>
-                                    <td>{launch.rocket.second_stage.payloads[0].orbit}</td>
-                                    <td>
-                                        {
-                                            launch.upcoming === true ?<p style={{backgroundColor:'orange',color:'red',width:'auto',borderRadius:'12px'}}> Upcoming </p>:
-                                            launch.launch_success ? <p style={{backgroundColor:'lightgreen',color:'green',width:'auto',borderRadius:'12px'}}>{success}</p> : <p style={{backgroundColor:'orange',color:'red',width:'auto',borderRadius:'12px'}}>{fail}</p>
-                                        }
-                                    </td>
-                                    <td>{launch.rocket.rocket_name}</td>
-                                </tr>
-                            )
-                        }) : null
-                    }                    
-                </tbody>
-            </Table>
+          { launches ?
+            <BootstrapTable  rowEvents={rowEvent} keyField='flight_number' data={paginateData} columns={paginateColumns} pagination={pagination}/>: null}
             {
                 selectedData ?
                 <Modal show={show} onHide={handleClose} animation={false}>
@@ -168,7 +164,6 @@ export default function Dashboard() {
                                     </p>
                                 </Card.Title>
                                 <Card.Subtitle className="mb-2 text-muted">{selectedData.rocket.rocket_name}</Card.Subtitle>
-                                
                             </Card.Body>
                         </Card>
                     </Modal.Title>
